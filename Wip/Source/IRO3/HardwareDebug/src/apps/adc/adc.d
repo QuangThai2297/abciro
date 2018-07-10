d23154b096e3dff0ef1064d79bb1d971348f5900
QuanVu <quanvuee@gmail.com>
2018-07-10 17:54:58 +0700

    add project

---
+++ Wip/Source/IRO3/HardwareDebug/src/apps/adc/adc.d
@@ -0,0 +1,31 @@
+src/apps/adc/adc.obj src/apps/adc/adc.d: ../src/apps/adc/adc.c
+src/apps/adc/adc.obj src/apps/adc/adc.d: C:\PROGRA~2\Renesas\RX\2_7_0\include\builtin.h
+C:\PROGRA~2\Renesas\RX\2_7_0\include\builtin.h:
+src/apps/adc/adc.obj src/apps/adc/adc.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\platform.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\platform.h:
+src/apps/adc/adc.obj src/apps/adc/adc.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\./board/generic_rx130/r_bsp.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\./board/generic_rx130/r_bsp.h:
+src/apps/adc/adc.obj src/apps/adc/adc.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/all/r_bsp_common.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/all/r_bsp_common.h:
+src/apps/adc/adc.obj src/apps/adc/adc.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_config\r_bsp_config.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_config\r_bsp_config.h:
+src/apps/adc/adc.obj src/apps/adc/adc.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/register_access/iodefine.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/register_access/iodefine.h:
+src/apps/adc/adc.obj src/apps/adc/adc.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/mcu_info.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/mcu_info.h:
+src/apps/adc/adc.obj src/apps/adc/adc.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/mcu_locks.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/mcu_locks.h:
+src/apps/adc/adc.obj src/apps/adc/adc.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/locking.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/locking.h:
+src/apps/adc/adc.obj src/apps/adc/adc.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/cpu.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/cpu.h:
+src/apps/adc/adc.obj src/apps/adc/adc.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/mcu_init.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/mcu_init.h:
+src/apps/adc/adc.obj src/apps/adc/adc.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/mcu_interrupts.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/mcu_interrupts.h:
+src/apps/adc/adc.obj src/apps/adc/adc.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\board/generic_rx130/hwsetup.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\board/generic_rx130/hwsetup.h:
+src/apps/adc/adc.obj src/apps/adc/adc.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\board/generic_rx130/lowsrc.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\board/generic_rx130/lowsrc.h:
+src/apps/adc/adc.obj src/apps/adc/adc.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\board/generic_rx130/vecttbl.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\board/generic_rx130/vecttbl.h:
---
+++ Wip/Source/IRO3/HardwareDebug/src/apps/timer/timer.d
@@ -0,0 +1,31 @@
+src/apps/timer/timer.obj src/apps/timer/timer.d: ../src/apps/timer/timer.c
+src/apps/timer/timer.obj src/apps/timer/timer.d: C:\PROGRA~2\Renesas\RX\2_7_0\include\builtin.h
+C:\PROGRA~2\Renesas\RX\2_7_0\include\builtin.h:
+src/apps/timer/timer.obj src/apps/timer/timer.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\platform.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\platform.h:
+src/apps/timer/timer.obj src/apps/timer/timer.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\./board/generic_rx130/r_bsp.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\./board/generic_rx130/r_bsp.h:
+src/apps/timer/timer.obj src/apps/timer/timer.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/all/r_bsp_common.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/all/r_bsp_common.h:
+src/apps/timer/timer.obj src/apps/timer/timer.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_config\r_bsp_config.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_config\r_bsp_config.h:
+src/apps/timer/timer.obj src/apps/timer/timer.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/register_access/iodefine.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/register_access/iodefine.h:
+src/apps/timer/timer.obj src/apps/timer/timer.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/mcu_info.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/mcu_info.h:
+src/apps/timer/timer.obj src/apps/timer/timer.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/mcu_locks.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/mcu_locks.h:
+src/apps/timer/timer.obj src/apps/timer/timer.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/locking.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/locking.h:
+src/apps/timer/timer.obj src/apps/timer/timer.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/cpu.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/cpu.h:
+src/apps/timer/timer.obj src/apps/timer/timer.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/mcu_init.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/mcu_init.h:
+src/apps/timer/timer.obj src/apps/timer/timer.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/mcu_interrupts.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\mcu/rx130/mcu_interrupts.h:
+src/apps/timer/timer.obj src/apps/timer/timer.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\board/generic_rx130/hwsetup.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\board/generic_rx130/hwsetup.h:
+src/apps/timer/timer.obj src/apps/timer/timer.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\board/generic_rx130/lowsrc.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\board/generic_rx130/lowsrc.h:
+src/apps/timer/timer.obj src/apps/timer/timer.d: D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\board/generic_rx130/vecttbl.h
+D:\Project\IRO3\Wip\Source\IRO3\src\smc_gen\r_bsp\board/generic_rx130/vecttbl.h:
